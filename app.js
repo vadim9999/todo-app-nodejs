a9b9cac7e2f5c85b0c990b846b93525ff1d73536
Vadim <vadim.putrov@gmail.com>
2019-08-15 14:52:22 +0300

    add connection to db

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser');
 const task = require('./routes/task.route')
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://localhost:27017/test';
+// let dev_db_url = 'mongodb://myUserAdmin:abc123@localhost:27017/test';
+// let dev_db_url = 'mongodb://someuser:abcd1234@ds123619.mlab.com:23619/productstutorial';
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/tasks', task)
 let port = 1234;
 
